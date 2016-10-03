adfc9386fadb4172f930eda0c5a084874c83a7b6
Daniel Rech <dr@netsyno.com>
2016-10-03 18:27:36 +0200

    some refactorings to make send_email more reusable

--- iPad-Search.py
+++ iPad-Search.py
@@ -1,6 +1,7 @@
 ### Dedicated to Daniel ###
 # -*- coding: utf-8 -*-
 import urllib
+import smtplib
 
 ######## Webseite auslesen
 sock = urllib.urlopen("http://www.apple.com/de/shop/browse/home/specialdeals/ipad/ipad_air_2/")
@@ -10,20 +11,19 @@ sock.close()
 ########## Webseite durchsuchen
 iPad128 = htmlSource.find("Generalüberholtes iPad Air 2 Wi-Fi 128 GB – Silber”)
 
-
 ########## E-Mail versenden
-import smtplib
+def send_email():
+    message128 = """From: From Person <from@fromdomain.com>
+To: To Person <to@todomain.com>
+Subject: iPad 128GB verfügbar!
 
-message128 = """From: From Person <from@fromdomain.com>
-			To: To Person <to@todomain.com>
-			Subject: iPad 128GB verfügbar!
-
-			E-Mail Body.
-			“""
+E-Mail Body.
+“""
+    smtpObj = smtplib.SMTP('mx.freenet.de:587')
+    smtpObj.starttls()
+    smtpObj.login('***@freenet.de’,’************')
+    smtpObj.sendmail('***@freenet.de','***@freenet.de',message128)
+    smtpObj.close()
 
 if iPad128 != -1 :
-       smtpObj = smtplib.SMTP('mx.freenet.de:587')
-       smtpObj.starttls()
-       smtpObj.login('***@freenet.de’,’************')
-       smtpObj.sendmail('***@freenet.de','***@freenet.de',message128)
-       smtpObj.close()
+    send_email()
